9a43055a5c840b19971d01b35bf97275647aa91a
Peng Wang <pwang@nata.com.au>
2018-11-15 16:30:55 +1100

    More in SanityCheck.sql

--- ProcessDuplicatePDTProj/SanityCheck.sql
+++ ProcessDuplicatePDTProj/SanityCheck.sql
@@ -4,3 +4,12 @@ select name from nata.Technique group by name having count(*) > 1			-- 0
 
 select ProductId, ActivityServiceId from nata.ServiceProduct
 group by ProductId, ActivityServiceId having count(*) > 1
+
+select DeterminationId, ActivityServiceId from nata.ServiceDetermination
+group by DeterminationId, ActivityServiceId having count(*) > 1
+
+select DeterminationId, ActivityServiceId, count(*) from nata.ServiceDetermination sd
+group by DeterminationId, ActivityServiceId having count(*) > 1
+
+select DeterminationId, TechniqueId, ServiceDeterminationId,  count(*) from nata.DeterminationTechnique dt
+group by DeterminationId, TechniqueId, ServiceDeterminationId having count(*) > 1
